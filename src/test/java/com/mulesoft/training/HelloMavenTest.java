ed74b5e650e928130a1be4c501ba8f7d86e38a11
Oswaldo Perez <oswaldo.perez@das.ohio.gov>
2017-11-06 17:00:24 -0500

    Test case addded

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -20,6 +21,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 
 }
